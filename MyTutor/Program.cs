e96c5d7bbfbe35f3f109482a7e8a65474f58c628
Karetskas_Evgeny <kub99999@yandex.ru>
2022-08-16 19:13:46 +0500

    Add project files.

---
+++ MyTutor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyTutor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
